a75563c12d512baff8199d28a2e76f341e45cf18
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-09 19:08:12 +0500

    nth

--- if01.py
+++ if01.py
@@ -1,9 +1,6 @@
 def main(a):
-    a=int(input())
     if a>0:
         print(a+1)
-    else:
-        print(a)
     """
     If the number is positive, increase it to 1, else leave unchanged.
     Args:
@@ -11,5 +8,5 @@ def main(a):
     Returns:
         a: a increased by 1 if positive, else unchanged.
     """
-    return a
-print(main(4))
+    return 
+print(main(-2))
