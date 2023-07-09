0c2d93683260668437eff173ece7322b24ae7416
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-09 22:39:00 +0500

    nth

--- if09.py
+++ if09.py
@@ -1,13 +1,12 @@
 def main(a):
-    a>0
     c=a%10
     b=a//10
     d=c*10+b
     if a>=d:
-        return 'True'
+        return "True"
     else: 
-        return 'False'
-print(main(67))
+        return "False"
+print(main(55))
 """
     The two-digit integer is given.
     Replace the digits of the number.
