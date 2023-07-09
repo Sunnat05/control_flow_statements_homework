45dd83d0294b9859a364eac256b020a9e4a4490d
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-09 21:45:16 +0500

    nth

--- if08.py
+++ if08.py
@@ -5,9 +5,9 @@ def main(a):
         return "two-digit even number"
     elif a%2==1 and a//1000==0 and not a//100==0 and not a//10==0 and not a//1==0:
         return "three-digit odd number"
-    elif a%2==0 and  a//000==0 and not a//100==0 and not a//10==0 and not a//1==0:
+    elif a%2==0 and  a//1000==0 and not a//100==0 and not a//10==0 and not a//1==0:
         return "three-digit even number"
-print(main(999))
+print(main(9))
 """
     Given an integer a, check the following conditions:
     "two-digit odd number",
