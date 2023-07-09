8acc851d4a04fcd22175b8a5ec95fb76f8b1a8c5
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-09 21:14:27 +0500

    nth

--- if07.py
+++ if07.py
@@ -1,5 +1,16 @@
 def main(a):
-    """
+    if a>0 and a%2==1:
+        return "positive odd number"
+    elif a>0 and a%2==0:
+        return "positive even number"
+    elif a<0 and a%2==1:
+        return "negative odd number"
+    elif a<0 and a%2==0:
+        return "negateve even number"
+    elif a==0:
+        return "the number is zero"
+print(main(-8))
+"""
     Given an integer a, check the following conditions:
     "positive odd number",
     "positive even number",
@@ -12,4 +23,3 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
