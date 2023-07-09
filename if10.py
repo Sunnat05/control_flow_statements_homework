5894bfe43b5eb05c9460c3646f303e2742a534ca
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-09 22:11:17 +0500

    nth

--- if10.py
+++ if10.py
@@ -1,5 +1,18 @@
 def main(temp):
-    """
+    if temp<0:
+        return "Freezing"
+    elif 1<=temp<=10:
+        return "Very Cold"
+    elif 11<=temp<=20:
+        return "Cold"
+    elif 21<=temp<=30:
+        return "Normal"
+    elif 31<=temp<=40:
+        return "Hot"
+    elif temp>40:
+        return "Very Hot"
+print(main(42))
+"""
     Display the message according to the following temperature conditions given to you in Celsius:
     Temp<0: "Freezing"
     Temp 1-10: "Very Cold"
@@ -13,4 +26,3 @@ def main(temp):
     Returns:
         string: the message to print
     """
-    return
