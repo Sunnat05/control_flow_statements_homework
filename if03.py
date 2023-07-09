9c6e77f9254360fdbf5cc94a751cbd6275d8dd9c
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-09 19:59:33 +0500

    nth

--- if03.py
+++ if03.py
@@ -1,9 +1,16 @@
 def main(a):
-    """
+    if a>0:
+        return a+1
+    elif a<0:
+        return a-2
+    elif a==0:
+         return 10
+print(main(1))
+"""
     If the number is positive, increase it to 1, else decrease it to 2. If it is 0, assign 10.
     Args:
         a: integer
     Returns:
         a: integer
     """
-    return
+    
