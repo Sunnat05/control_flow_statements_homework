6f45da5d93fa955d7d2091b05279941e39aa9bf8
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-10 09:49:28 +0500

    nth

--- if04.py
+++ if04.py
@@ -6,7 +6,7 @@ def main(a,b,c):
         s+=1
     if c>0:
         s+=1
-        return s
+    return s
 print(main(2,-4,9))
 """
     Find how many positive numbers there are in the given numbers.
